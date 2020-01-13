6073e5efd88daeaddc865f6a1cb032355c3fcec0
RahulKadgekar <RahulKadgekar@LAPTOP-CVRDL1P1>
2020-01-13 11:54:22 -0500

    helloworld controller

---
+++ src/main/java/com/example/spring/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.spring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+	
+	@GetMapping("/hello")
+	public String sayHello(String name) {
+		return "hello";
+	}
+}
