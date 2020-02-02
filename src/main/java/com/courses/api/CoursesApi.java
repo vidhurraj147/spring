56b7fb2b8d2dce10efba1904fae90035951a58a8
RahulKadgekar <RahulKadgekar@LAPTOP-CVRDL1P1>
2020-02-01 20:37:55 -0500

    spring examples

--- src/main/java/com/courses/api/CoursesApi.java
+++ src/main/java/com/courses/api/CoursesApi.java
@@ -3,11 +3,11 @@ package com.courses.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class CoursesApi {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CoursesApi.class, args);
-	}
+//	public static void main(String[] args) {
+//		SpringApplication.run(CoursesApi.class, args);
+//	}
 
 }
--- src/main/java/com/example/spring/Application.java
+++ src/main/java/com/example/spring/Application.java
@@ -1,7 +1,10 @@
 package com.example.spring;
 
+import java.text.NumberFormat;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -9,5 +12,12 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+	@Bean
+	public NumberFormat defaultCurrentFormat() {
+		return NumberFormat.getCurrencyInstance();
+	}
+	
+	
 
 }
